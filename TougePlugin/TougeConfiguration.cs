e51c63a4b090a83ed2111bbeb3c34b87f345bc77
Eric <71130341+EricManintveld@users.noreply.github.com>
2025-06-20 17:54:42 +0200

    Remove unused configuration variable.

--- TougePlugin/TougeConfiguration.cs
+++ TougePlugin/TougeConfiguration.cs
@@ -42,9 +42,6 @@ public class TougeConfiguration : IValidateConfiguration<TougeConfigurationValid
     [YamlMember(Description = "The ruleset used for the touge sessions. Options: BattleStage or CatAndMouse.")]
     public RulesetType RuleSetType = RulesetType.BattleStage;
 
-    [YamlMember(Description = "The type or race. Options: Course or Outrun.")]
-    public RaceType RaceType = RaceType.Course;
-
     [YamlMember(Description = "Discrete mode for the hud. Only shows the hud when necassary, hidden otherwise.")]
     public bool DiscreteMode = false;
 
