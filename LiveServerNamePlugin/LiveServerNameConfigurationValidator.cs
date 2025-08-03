b36631c5bb5d89015de0ac222f1ed7961a8aa467
Ereebay <xk.jackson@hotmail.com>
2025-08-03 22:15:16 +0800

    add live server name plugin

---
+++ LiveServerNamePlugin/LiveServerNameConfigurationValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace LiveServerNamePlugin;
+
+public class LiveServerNameConfigurationValidator : AbstractValidator<LiveServerNameConfiguration>
+{
+    public LiveServerNameConfigurationValidator()
+    {
+        RuleFor(cfg => cfg.UpdateInterval).NotNull().LessThanOrEqualTo(7200).GreaterThanOrEqualTo(1);
+        RuleFor(cfg => cfg.Randomize).NotNull();
+        RuleFor(cfg => cfg.ListOfNames).NotNull();
+    }
+}
